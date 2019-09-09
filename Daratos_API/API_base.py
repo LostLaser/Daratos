aa7cbf312b116df52e645866b044fecef7b89553
LostLaser <jtgolds6@gmail.com>
2019-09-08 20:12:35 -0500

    Off loading prediction code from API to improve readability

--- Daratos_API/API_base.py
+++ Daratos_API/API_base.py
@@ -2,28 +2,12 @@ import flask
 from flask import request, jsonify
 from flask import Flask
 from flask import request
-from keras.models import load_model
-import numpy
-import keras
-import tensorflow as tf
 import sys
-import TextProcessor
 import API_Exceptions
+import BiasPrediction
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-#Loading items needed for prediction
-try:
-    full_processor = TextProcessor.ProcessRaw()
-except OSError:
-    full_processor = None
-try:
-    model = load_model('sentenceModel.h5')
-    model._make_predict_function()
-except IOError:
-    model = None
-graph = tf.get_default_graph()
 
 @app.route('/', methods=['GET'])
 def info():
@@ -31,7 +15,7 @@ def info():
 
 @app.route('/health', methods=['GET'])
 def home():
-    return "<p>We are up!</p>"
+    return "We are up!"
 
 @app.route('/bias', methods=['GET'])
 def bias_calc():
@@ -42,22 +26,14 @@ def bias_calc():
         Json object of the bias details
     '''
     ret_val = {}
-    content = request.args.get('content',type = str)
-
+    content = request.args.get('content', type = str)
     if len(content) == 0:
-        return jsonify(ret_val) 
-    if model is None or full_processor is None:
-        raise API_Exceptions.InvalidUsage('Missing prediction resources', status_code=404)
-    
-    #Tokenizing the sentences that are inside of input content
-    content_sentences = full_processor.split_sentences(content)
-    tokenized_sentences = list(map(full_processor.full_clean, content_sentences))
-
-    #Making predictions on each of the sentences
-    predictions = []
-    with graph.as_default():
-        for i in range(len(tokenized_sentences)):
-            predictions.extend(model.predict(tokenized_sentences[i]).tolist())
+        return jsonify(ret_val)
+        
+    try:
+        predictions, _ = BiasPrediction.predict_article(content)
+    except EnvironmentError:
+        API_Exceptions.InvalidUsage('Missing prediction resources', status_code = 404)
 
     total_bias = 0.85
     ret_val = {'total_bias': total_bias,
