d92c5dd1f395fca14f15885c7b5918b364740d8b
LostLaser <jtgolds6@gmail.com>
2019-09-16 23:22:04 -0500

    Adding error checks on extension fetch calls

--- Daratos_EXT/popup/display.js
+++ Daratos_EXT/popup/display.js
@@ -1,6 +1,9 @@
-//createCanvas()
+function setPopupMessage(output_message) {
+    document.getElementById("output_box").innerHTML = output_message;
+    document.getElementById("loader").hidden = true;
+}
+
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("setup");
     
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
@@ -8,16 +11,31 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log(word);
 
             let url = "http://127.0.0.1:5000/bias?content="+String(word);
-            async function getUserAsync() 
-            {
-                let response = await fetch(url);
-                let data = await response.json()
-                return data;
+            function getBias() {
+                if (word.length > 0) {
+                    fetch(url)
+                        .then(
+                            function(response) {
+                                if (response.status !== 200) {
+                                    setPopupMessage("Whoops! Looks like there was a problem.")
+                                    console.log('Looks like there was a problem. Status Code: ' +
+                                    response.status);
+                                    return;
+                                }
+                                response.json().then(function(data) {
+                                    setPopupMessage(data.total_bias);
+                                });
+                        })
+                        .catch(setPopupMessage("Oh no! Unable to connect to bias predictor."));
+                }
+                else {
+                    setPopupMessage("No content found!")
+                }
             }
 
-            getUserAsync()
-                .then(data => document.getElementById("output_box").innerHTML = data.lean);
+            getBias();
         });
     });
     
 });
+
