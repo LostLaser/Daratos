156d94b854666e44d572b141f93eeca674bbf9a4
iberns <ianberns1@gmail.com>
2019-10-02 15:39:35 -0500

    Generic NBC web article scraper

--- Daratos_API/Webscraper/testScraper.py
+++ Daratos_API/Webscraper/testScraper.py
@@ -27,9 +27,45 @@ class WebDriver(webdriver.Chrome):
             self.quit()
             print("Error loading website {}.. \n".format(url) + str(e))
 
+    def getTitle(self):
+        try:
+            return self.driver.title
+        except Exception as e:
+            self.quit()
+            return "Unable to extract title from news article. \n {}".format(str(e))
 
-headless = False
+    def getAuthor(self):
+        try:
+            author = self.driver.find_element_by_xpath("//div[contains(text(), 'By')]").text
+            author = author.replace('By ', '')
+            return author
+        except Exception as e:
+            return "Error in getting author... \n" + str(e)
+
+    def getBody(self):
+        try:
+            bodyText = ""
+            itemList = self.driver.find_elements_by_xpath('//div/p')
+            for item in itemList:
+                bodyText += item.text + '\n'
+            return bodyText
+        except Exception as e:
+            bodyText += "Error in grabbing body text.. \n" + str(e)
+            return bodyText
+
+
+headless = True
 url = "https://www.nbcnews.com/politics/donald-trump/pompeo-admits-he-was-controversial-call-between-trump-ukrainian-president-n1061101"
+url = input("Please enter an NBC article URL: ")
 
 wd = WebDriver(headless)
 wd.loadWebsite(url)
+
+titleText = wd.getTitle()
+bodyText = wd.getBody()
+authorText = wd.getAuthor()
+wd.quit()
+
+open('log.txt', 'w').close()
+article = {"Title": titleText, "Author": authorText, "Body": bodyText}
+print(article,  file=open('log.txt', 'w'))
