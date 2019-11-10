620a2b32b53281714b21f5e794f755f6a68e4981
LostLaser <jtgolds6@gmail.com>
2019-11-10 14:15:24 -0600

    Adding a cleaner interface for extension

--- Daratos_EXT/popup/display.js
+++ Daratos_EXT/popup/display.js
@@ -5,54 +5,57 @@ document.addEventListener('DOMContentLoaded', function () {
 function fetch_bias(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         let pathArray = tabs[0].url.split('/');
-        let host = pathArray[2];
-        let host_xpath = "//div/p";
-        // call_api("http://127.0.0.1:5000/article/xpath", {
-        //         method: 'post',
-        //         body: JSON.stringify({host: String(host)}),
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         }
-        //     }).then(function(response){    
-        //         host_xpath = response.host_xpath
-        //     });
-        if (! host_xpath) {
-            setPopupMessage("This website is not supported yet.");
-            return
+        let domain = pathArray[2];
+        let domain_xpath = "";
+        call_options = {
+            method: 'post',
+            body: JSON.stringify({domain: String(domain)}),
+            headers: {'Content-Type': 'application/json'}
         }
-        
-        chrome.tabs.sendMessage(tabs[0].id, {xpath: host_xpath}, function(response) {
-            let web_content = response.text_content;
+        call_api("http://127.0.0.1:5000/article/xpath", call_options).then(function(response){    
+            if (! response) {
+                return
+            }
 
-            setLoading();
+            domain_xpath = response.domain_xpath
+
+            if (! domain_xpath) {
+                setPopupMessage("This website is not supported yet.");
+                return
+            }
             
-            if (web_content) {
-                call_api("http://127.0.0.1:5000/bias", {
-                    method: 'post',
-                    body: JSON.stringify({content: String(web_content)}),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then(function(response){    
-                    if (response.total_bias) {
-                        setPopupMessage(response.total_bias);
-                    }
-                    else {
-                        setPopupMessage("Oh no! Something went wrong.")
-                    }
-                });
-            }
-            else {
-                setPopupMessage("No content found!");
-            }
+            chrome.tabs.sendMessage(tabs[0].id, {xpath: domain_xpath}, function(response) {
+                let web_content = response.text_content;
+                
+                if (web_content) {
+                    bias_call_options = {
+                        method: 'post',
+                        body: JSON.stringify({content: String(web_content)}),
+                        headers: {'Content-Type': 'application/json'}
+                    }   
+                    call_api("http://127.0.0.1:5000/bias", bias_call_options).then(function(response){    
+                        if (response.total_bias) {
+                            setPopupMessage(response.total_bias);
+                        }
+                        else {
+                            setPopupMessage("Oh no! Something went wrong.")
+                        }
+                    });
+                }
+                else {
+                    setPopupMessage("No content found!");
+                }
+            });
         });
     });
 }
 
 // safely call api with specified options
 async function call_api(url, options) {
-    var return_val;
+    setLoading();
     
+    var return_val;
+
     let response = await fetch(url, options)
         .catch(function(response) {
             console.log(response)
@@ -74,13 +77,13 @@ async function call_api(url, options) {
 }
 
 function setPopupMessage(output_message) {
-    document.getElementById("bias_button").hidden = true;
+    document.getElementById("bias_button").classList.add("hide")
     document.getElementById("output_box").innerHTML = output_message;
-    document.getElementById("loader").hidden = true;
+    document.getElementById("loader").classList.add("hide")
 }
 
 function setLoading() {
-    document.getElementById("bias_button").hidden = true;
+    document.getElementById("bias_button").classList.add("hide")
     document.getElementById("output_box").innerHTML = "";
-    document.getElementById("loader").hidden = false;
+    document.getElementById("loader").classList.remove("hide")
 }
