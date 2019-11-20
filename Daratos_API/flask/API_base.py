159a375fa113ae94882309812d4951c3fcb807ea
LostLaser <jtgolds6@gmail.com>
2019-11-19 19:05:22 -0600

    Adding docker base files

--- Daratos_API/flask/API_base.py
+++ Daratos_API/flask/API_base.py
@@ -1,19 +1,18 @@
 import sys
 import flask
-from flask import request, jsonify
-from flask import Flask
-from flask import request
+from flask import request, jsonify, Flask
+
+import db_handler
 import api_exception
 import bias_prediction
-# import news_scraper
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-# scraper = news_scraper.WebDriver(True)
+
 
 @app.route('/', methods=['GET'])
 def info():
-    return 'Welcome to the political bias API! Go to /bias if you are wanting to get a rating!'
+    return 'Welcome to the political bias API! Go to /bias if you are wanting to get a prediction!'
 
 @app.route('/health', methods=['GET'])
 def home():
@@ -24,19 +23,23 @@ def bias_calc():
     '''
     Endpoint to determine the bias of the specified news article
 
+    Parameters: 
+        content (str): String containing English sentences
+        verbose (bool): truth value make call return more detailed information
+
     Returns:
         Json object of the bias details
     '''
     if not request.json:
         raise api_exception.InvalidUsage('Missing json request body', status_code = 400)
-
+    
     json_data = request.get_json()
     content = str(json_data.get('content'))
     verbose = str(json_data.get('verbose'))
     
     if not content or len(content) == 0:
         raise api_exception.InvalidUsage('No content specified', status_code = 204)
-
+    
     try:
         predictions, sentences = bias_prediction.predict_article(content)
     except EnvironmentError:
@@ -46,8 +49,8 @@ def bias_calc():
     if verbose and verbose.lower() == 'true':
         prediction_info = []
         for i in range( min(len(sentences), len(predictions)) ):
-            prediction_info.append( { 'sentence': sentences[i],
-                                      'prediction': predictions[i]})
+            prediction_info.append({ 'sentence': sentences[i],
+                                     'prediction': predictions[i] })
 
         ret_val = {'total_bias': total_bias,
                    'prediction_info': prediction_info}
@@ -61,21 +64,31 @@ def retrieve_xpath():
     '''
     Endpoint to determine the web scraping details of a news website
 
+    Parameters: 
+        domain (str): domain name of a supported news website
+
     Returns:
         Json object containing the xpath
     '''
     json_data = request.get_json()
-    host = str(json_data.get('host'))
-    x_path = ''
-    print(host)
-    if host == 'www.foxnews.com':
-        x_path = '//div/p'
+    domain = str(json_data.get('domain'))
+    
+    x_path = db_handler.get_xpath(domain)
 
-    ret_val = {'host_xpath': x_path}
+    ret_val = {'domain_xpath': x_path}
     return jsonify(ret_val)
 
 @app.route('/tokenize', methods=['GET'])
 def tokenize_sentences():
+    '''
+    Endpoint to tokenize a string the way Daratos does
+
+    Parameters: 
+        content (str): String containing English sentences
+  
+    Returns: 
+        Json object containing the tokenized sentences
+    '''
     content = request.form.get('content', type = str)
     
     sentence_fragments = bias_prediction.tokenize_sentences(content)
