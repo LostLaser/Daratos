98e82eeee67a1b312a26eee6ff11ee3ad3a35aad
LostLaser <jtgolds6@gmail.com>
2020-01-06 17:49:17 -0600

    Removing unnecessary files after prediction source switch

--- Daratos_API/flask/app.py
+++ Daratos_API/flask/app.py
@@ -3,10 +3,8 @@ from flask import request, jsonify, Flask
 
 import db_handler
 import api_exception
-import bias_prediction
 import config
 
-prediction_instance = bias_prediction.Predictor()
 app = Flask(__name__)
 
 @app.route('/')
@@ -34,28 +32,11 @@ def bias_calc():
     
     json_data = request.get_json()
     content = str(json_data.get('content'))
-    verbose = str(json_data.get('verbose'))
     
     if not content or len(content) == 0:
         raise api_exception.InvalidUsage('No content specified', status_code = 204)
     
-    try:
-        predictions, sentences = prediction_instance.predict_article(content)
-    except EnvironmentError:
-        raise api_exception.InvalidUsage('Missing prediction resources', status_code = 503)
-    
-    total_bias = prediction_instance.consolidate_biases(predictions)
-    
-    if verbose and verbose.lower() == 'true':
-        prediction_info = []
-        for i in range( min(len(sentences), len(predictions)) ):
-            prediction_info.append({ 'sentence': sentences[i],
-                                     'prediction': predictions[i] })
-
-        ret_val = {'total_bias': total_bias,
-                   'prediction_info': prediction_info}
-    else:
-        ret_val = {'total_bias': total_bias}
+    ret_val = ""
     
     return jsonify(ret_val)
 
@@ -78,29 +59,6 @@ def retrieve_xpath():
         x_path = db_handler.get_xpath(domain)
 
     ret_val = {'domain_xpath': x_path}
-    return jsonify(ret_val)
-
-@app.route('/tokenize', methods=['GET'])
-def tokenize_sentences():
-    '''
-    Endpoint to tokenize a string the way Daratos does
-
-    Parameters: 
-        content (str): String containing English sentences
-  
-    Returns: 
-        Json object containing the tokenized sentences
-    '''
-    content = request.form.get('content', type = str)
-    
-    sentence_fragments = prediction_instance.tokenize_sentences(content)
-    output_tokens = []
-
-    for sentence in sentence_fragments:
-        output_tokens.append(sentence[0].tolist())
-
-    ret_val = {'tokenized_sentences': output_tokens}
-
     return jsonify(ret_val)
 
 @app.errorhandler(api_exception.InvalidUsage)
