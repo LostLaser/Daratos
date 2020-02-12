339ece76376021c2dc68a1522423473f24544f45
LostLaser <jtgolds6@gmail.com>
2020-02-11 21:32:22 -0600

    Changing prediction api url

--- Daratos_API/config.py
+++ Daratos_API/config.py
@@ -4,7 +4,7 @@ import multiprocessing
 PORT = int(env.get("PORT", 8080))
 DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
 DB_CONFIG_PATH = str(env.get("DB_CONFIG_PATH", "./db_config.json"))
-PREDICTION_API_URL = str(env.get("PREDICTION_URL","https://vote-public2-fernandotoress.cloud.okteto.net"))
+PREDICTION_API_URL = str(env.get("PREDICTION_URL","https://api.thebipartisanpress.com/api/endpoints/daratos"))
 PREDICTION_API_KEY = str(env.get("PREDICTION_API_KEY", ""))
 if not PREDICTION_API_KEY:
     print("ERROR: Missing PREDICTION_API_KEY" + PREDICTION_API_KEY)
