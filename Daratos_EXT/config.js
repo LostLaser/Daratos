f2fbf031c2d8372c8b09e18d3051828dd16867f1
LostLaser <jtgolds6@gmail.com>
2020-04-07 21:55:53 -0500

    Improving error messaging in extension

--- Daratos_EXT/config.js
+++ Daratos_EXT/config.js
@@ -1,11 +1,14 @@
 var config = {
-    "daratos_api_url": "http://127.0.0.1:8080"
+    "daratos_api_url": "http://127.0.0.1:8080",
+    "ERR_GENERIC": "Oh no! Something went wrong.",
+    "ERR_NO_CONTENT": "No content found!",
+    "ERR_RETRY": "There was a problem, please try again.",
+    "ERR_BIAS_CONNECTION": "Oh no! Unable to connect to bias predictor."
 };
 
 // production variables
 chrome.management.getSelf(function(result){
     var env = result.installType
-    
     if (env != "development") {
         config["daratos_api_url"] = "http://34.69.252.185"
     }
