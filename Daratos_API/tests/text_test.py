0e8cbf8a37be6202e29e6521b21dc251285e1a32
LostLaser <jtgolds6@gmail.com>
2020-05-06 22:29:32 -0500

    Adding in bias test case

--- Daratos_API/tests/text_test.py
+++ Daratos_API/tests/text_test.py
@@ -2,11 +2,3 @@ import unittest
 
 from app import app
 
-class TextTestCases(unittest.TestCase):
-    def test_home(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-if __name__ == '__main__':
-    unittest.main()
